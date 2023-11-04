9da6a07b52ed1e02ba6d672a3cf2da36ba8af2eb
RouryR <Rouryffx@hotmail.com>
2023-11-04 13:46:33 -0600

    proyecto grupo 1
    inicio del proyecto 1 de base de datos

---
+++ Grupo1/src/main/java/com/fidelitas/grupo1/Grupo1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.fidelitas.grupo1;
+
+/**
+ *
+ * @author RouryR
+ */
+public class Grupo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
